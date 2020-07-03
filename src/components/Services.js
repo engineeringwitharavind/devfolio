c2c5ee78ad943f6b521c850a8f8978c8554430da
aravind-alpha <s.aravindviews@gmail.com>
2020-07-03 17:31:19 +0530

    Updated skills component and styling

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,7 @@ import services from "../constants/services";
 const Services = () => {
   return (
     <section className="section bg-grey">
-      <Title title="services" />
+      <Title title="skills" />
       <div className="section-center services-center">
         {services.map((service) => {
           const { id, icon, title, text } = service;
