b108c11ed01ef0fc45bbcf1f7abd2cc2d88205ed
aravind-alpha <s.aravindviews@gmail.com>
2020-07-03 21:57:52 +0530

    Added Projects Page | Component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,34 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Image from "gatsby-image"
-import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
-}
+import React from "react";
+import PropTypes from "prop-types";
+import Image from "gatsby-image";
+import { FaGithub, FaLink } from "react-icons/fa";
+const Project = ({ description, title, github, stack, url, image, index }) => {
+  return (
+    <article className="project">
+      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      <div className="project-info">
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map((item) => {
+            return <span key={item.id}>{item.title}</span>;
+          })}
+        </div>
+        <div className="project-links">
+          <a href={github}>
+            <FaGithub className="project-icon" />
+          </a>
+          <a href={url}>
+            <FaLink className="project-icon" />
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
 
-Project.propTypes = {}
+// For Projects Number => <span className="project-number">0{index + 1}.</span>
 
-export default Project
+Project.propTypes = {};
+
+export default Project;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,46 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-import Projects from "../components/Projects"
+import React from "react";
+import Layout from "../components/Layout";
+import { graphql } from "gatsby";
+import Projects from "../components/Projects";
 // ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
-}
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects-page">
+        <Projects projects={projects} title="all projects" />
+      </section>
+    </Layout>
+  );
+};
 
-export default ProjectsPage
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default ProjectsPage;
