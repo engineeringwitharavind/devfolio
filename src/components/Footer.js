4d25f4414b92a4b6d87e7d3369efcff8e100b568
aravind-alpha <s.aravindviews@gmail.com>
2020-07-04 19:27:01 +0530

    Added footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
-import React from "react"
-import SocialLinks from "../constants/socialLinks"
-const Footer = () => {
-  return <h2>footer component</h2>
-}
+import React from "react";
+import SocialLinks from "../constants/socialLinks";
 
-export default Footer
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h6>
+          copyright&copy;{new Date().getFullYear()}
+          <span>Aravind</span> all rights reserved
+        </h6>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
