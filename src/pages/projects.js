34d26a6fe987a9b6ff284a1410211976e6fa7ee1
aravind-alpha <s.aravindviews@gmail.com>
2020-07-04 07:40:07 +0530

    Update Project component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 import Projects from "../components/Projects";
-// ...GatsbyImageSharpFluid
 
 const ProjectsPage = ({
   data: {
