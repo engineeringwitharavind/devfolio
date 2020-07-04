baa238a173438d9bc680f872c96c80ba91471776
aravind-alpha <s.aravindviews@gmail.com>
2020-07-04 19:23:03 +0530

    Updated Project|Blog components

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,7 +7,9 @@ const Blog = ({ id, title, image, date, category, slug, desc }) => {
   return (
     <Link to={`/blogs/${slug}`} className="blog" key={id}>
       <article>
-        <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        {image && (
+          <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        )}
         <div className="blog-card">
           <h4>{title}</h4>
           <p>{desc}</p>
@@ -21,6 +23,14 @@ const Blog = ({ id, title, image, date, category, slug, desc }) => {
   );
 };
 
-Blog.propTypes = {};
+Blog.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+};
 
 export default Blog;
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,9 @@ import { FaGithub, FaLink } from "react-icons/fa";
 const Project = ({ description, title, github, stack, url, image, index }) => {
   return (
     <article className="project">
-      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      {image && (
+        <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      )}
       <div className="project-info">
         <h3>{title}</h3>
         <p className="project-desc">{description}</p>
@@ -29,6 +31,13 @@ const Project = ({ description, title, github, stack, url, image, index }) => {
 
 // For Projects Number => <span className="project-number">0{index + 1}.</span>
 
-Project.propTypes = {};
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  github: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  stack: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Project;
