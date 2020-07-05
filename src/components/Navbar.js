8db390846254a4df20fdcb32615fd3d69471a4a2
aravind-alpha <s.aravindviews@gmail.com>
2020-07-05 22:26:06 +0530

    Updated Logo and Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.svg";
+import logo from "../assets/aravind-logo.svg";
 import { FaAlignRight } from "react-icons/fa";
 import PageLinks from "../constants/links";
 
@@ -8,7 +8,7 @@ const Navbar = ({ toggleSidebar }) => {
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} alt="logo" />
+          <img className="logo" src={logo} alt="logo" />
           <button type="button" className="toggle-btn" onClick={toggleSidebar}>
             <FaAlignRight></FaAlignRight>
           </button>
