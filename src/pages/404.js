374aae1790b6ee2aa1c0bf37d1275bff788c3ede
aravind-alpha <s.aravindviews@gmail.com>
2020-07-10 23:06:23 +0530

    Updated Project Component

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,10 @@ import SEO from "../components/SEO";
 const Error = () => {
   return (
     <Layout>
+      <SEO
+        title="404 Page"
+        description="Page is Not Available. Go to HOME Page"
+      />
       <main className="error-page">
         <h3>oops! it's a dead end</h3>
         <Link to="/" className="btn">
